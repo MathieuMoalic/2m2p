34816f6d7c7e18758d4de9a47bb370f9034a5285
Mathieu Moalic <mathieu.q.moalic@gmail.com>
2021-04-21 12:39:53 +0200

    .

--- lir/_make.py
+++ lir/_make.py
@@ -7,15 +7,17 @@ import multiprocessing as mp
 from typing import Tuple, List, Optional
 from tqdm.notebook import tqdm
 
-class Make:
 
+class Make:
     def _load_ovf(self, ovf_path: str) -> np.ndarray:
         """Returns an np.ndarray from the ovf"""
         with open(ovf_path, "rb") as f:
             for _ in range(28):
                 next(f)
             try:
-                arr: np.ndarray = np.fromfile(f, "<f4", count=self._count)[1:].reshape(self._ovf_shape)
+                arr: np.ndarray = np.fromfile(f, "<f4", count=self._count)[1:].reshape(
+                    self._ovf_shape
+                )
             except ValueError as e:
                 print("ovf files are corrupted")
                 return e
@@ -44,7 +46,7 @@ class Make:
                 #     dt = float(line.split("  ")[-2])
                 if "End: Header" in line:
                     break
-        parms = {'shape':(z,y,x,c), 'dx':dx, 'dy':dy, 'dz':dz}
+        parms = {"shape": (z, y, x, c), "dx": dx, "dy": dy, "dz": dz}
         return parms
 
     def parse_script(self) -> None:
@@ -57,21 +59,31 @@ class Make:
             value_match: Optional[Match[str]]
             region_match: Optional[Match[str]]
 
-            def get_attribute(key_re: str, value_re: str, region_re: str=None) -> None:
+            def get_attribute(
+                key_re: str, value_re: str, region_re: str = None
+            ) -> None:
                 key_match = re.search(key_re, line)
                 value_match = re.search(value_re, line)
                 # print(line,key_match,value_match,key_re,value_re)
                 if region_re is not None:
                     region_match = re.search(region_re, line)
 
-                    if isinstance(key_match, Match) and isinstance(value_match, Match) and isinstance(region_match, Match):
-                        key = region_match.group().split("_")[0] + "_" + key_match.group().replace(":=","").replace(" ","")
+                    if (
+                        isinstance(key_match, Match)
+                        and isinstance(value_match, Match)
+                        and isinstance(region_match, Match)
+                    ):
+                        key = (
+                            region_match.group().split("_")[0]
+                            + "_"
+                            + key_match.group().replace(":=", "").replace(" ", "")
+                        )
                         value = float(value_match.group())
                         f.attrs[key] = value
                         return True
 
                 elif isinstance(key_match, Match) and isinstance(value_match, Match):
-                    key = key_match.group().replace(":=","").replace(" ","")
+                    key = key_match.group().replace(":=", "").replace(" ", "")
                     value = float(value_match.group())
                     f.attrs[key] = value
                     return True
@@ -95,7 +107,7 @@ class Make:
                 if get_attribute(r"^[[f]|[f_cut]] *:=", r"\d+.?\d*e?-?\d*"):
                     continue
 
-    def add_table(self, table_path: str, dset_name: str="table") -> None:
+    def add_table(self, table_path: str, dset_name: str = "table") -> None:
         """Adds a the mumax table.txt file as a dataset"""
         if os.path.isfile(table_path):
             with open(table_path, "r") as table:
@@ -109,15 +121,15 @@ class Make:
         else:
             print("table.txt not found")
 
-    def _get_paths(self,load_path: str) -> Tuple[str,str]:
+    def _get_paths(self, load_path: str) -> Tuple[str, str]:
         """Cleans the input string and return the path for .out folder and .mx3 file"""
-        if load_path[-3:] in ['mx3','out']:
+        if load_path[-3:] in ["mx3", "out"]:
             load_path = load_path[:-4]
         out_path = f"{load_path}.out"
         mx3_path = f"{load_path}.mx3"
         return out_path, mx3_path
 
-    def add_mx3(self,mx3_path: str) -> None:
+    def add_mx3(self, mx3_path: str) -> None:
         """Adds the mx3 file to the f.attrs"""
         if os.path.isfile(mx3_path):
             with open(mx3_path, "r") as mx3:
@@ -141,55 +153,82 @@ class Make:
                         return True
         return False
 
-    def _get_dset_prefixes(self,out_path: str) -> List[str]:
+    def _get_dset_prefixes(self, out_path: str) -> List[str]:
         """From the .out folder, get the list of prefixes, each will correspond to a different dataset"""
-        prefixes = [i.split("/")[-1].replace("000000.ovf","") for i in glob(f"{out_path}/*00000.ovf")]
+        prefixes = [
+            i.split("/")[-1].replace("000000.ovf", "")
+            for i in glob(f"{out_path}/*00000.ovf")
+        ]
         if os.path.isfile(f"{out_path}/stable.ovf"):
             prefixes.append("stable")
         return prefixes
 
-    def _get_dset_name(self,prefix: str) -> str:
+    def _get_dset_name(self, prefix: str) -> str:
         """ From the prefix, this tries to return a human readable version """
-        common_prefix_to_name = (("zrange4","ND"),("zrange2","WG"),("stable","stable"))
+        common_prefix_to_name = (
+            ("zrange4", "ND"),
+            ("zrange2", "WG"),
+            ("stable", "stable"),
+        )
         for i in common_prefix_to_name:
             if i[0] in prefix:
                 return i[1]
         return prefix
 
-    def _save_stepsize(self,parms:dict) -> None:
-        for key in ['dx','dy','dz']:
-            if key not in self.list_attrs():
+    def _save_stepsize(self, parms: dict) -> None:
+        for key in ["dx", "dy", "dz"]:
+            if key not in self.attrs():
                 with h5py.File(self.h5_path, "a") as f:
                     f.attrs[key] = parms[key]
 
-    def add_dset(self, out_path: str, prefix: str, name:Optional[str] =None, tmax:Optional[int]=None, force:bool=False) -> None:
+    def add_dset(
+        self,
+        out_path: str,
+        prefix: str,
+        name: Optional[str] = None,
+        tmax: Optional[int] = None,
+        force: bool = False,
+    ) -> None:
         """Creates a dataset from an input .out folder path and a prefix (i.e. "m00")"""
         ovf_paths = sorted(glob(f"{out_path}/{prefix}*.ovf"))[:tmax]
         # load one file to initialize the h5 dataset with the correct shape
         self._ovf_parms = self._get_ovf_parms(ovf_paths[0])
         self._save_stepsize(self._ovf_parms)
-        self._ovf_shape = self._ovf_parms['shape']
-        dset_shape = ((len(ovf_paths),)+self._ovf_shape)
-        # number of bytes in the data used in self._load_ovf, (+1 is for the security number of ovf) 
-        self._count = self._ovf_shape[0] * self._ovf_shape[1] * self._ovf_shape[2] * self._ovf_shape[3] + 1
+        self._ovf_shape = self._ovf_parms["shape"]
+        dset_shape = (len(ovf_paths),) + self._ovf_shape
+        # number of bytes in the data used in self._load_ovf, (+1 is for the security number of ovf)
+        self._count = (
+            self._ovf_shape[0]
+            * self._ovf_shape[1]
+            * self._ovf_shape[2]
+            * self._ovf_shape[3]
+            + 1
+        )
         if name is None:
             name = self._get_dset_name(prefix)
-        
+
         with h5py.File(self.h5_path, "a") as f:
             if force and name in list(f.keys()):
                 del f[name]
             dset = f.create_dataset(name, dset_shape, np.float32)
             with mp.Pool(processes=int(mp.cpu_count())) as p:
-                for i, data in enumerate(tqdm(p.imap(self._load_ovf, ovf_paths), leave=False, desc=name, total=len(ovf_paths))):
+                for i, data in enumerate(
+                    tqdm(
+                        p.imap(self._load_ovf, ovf_paths),
+                        leave=False,
+                        desc=name,
+                        total=len(ovf_paths),
+                    )
+                ):
                     dset[i] = data
 
-    def add_np_dset(self,arr:np.ndarray,name:str,force:bool =False):
+    def add_np_dset(self, arr: np.ndarray, name: str, force: bool = False):
         if force and name in list(f.keys()):
             del f[name]
         with h5py.File(self.h5_path, "a") as f:
-            f.create_dataset(name,data=arr)
+            f.create_dataset(name, data=arr)
 
-    def make(self, load_path: str, tmax:Optional[int] =None) -> None:
+    def make(self, load_path: str, tmax: Optional[int] = None) -> None:
         """Automatically parse the load_path and will create datasets"""
         self._create_h5()
         out_path, mx3_path = self._get_paths(load_path)
@@ -201,4 +240,4 @@ class Make:
             pass
         dset_prefixes = self._get_dset_prefixes(out_path)
         for dset_prefix in dset_prefixes:
-            self.add_dset(out_path,dset_prefix,tmax=tmax)
+            self.add_dset(out_path, dset_prefix, tmax=tmax)
