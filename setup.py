17a0aa459ce3a9401386d7757f05fb19f0d795bc
Mathieu Moalic <mathieu.q.moalic@gmail.com>
2021-05-05 13:09:00 +0200

    fix requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="llyr",
-    version="0.1",
+    version="0.1.1",
     description="micromagnetic post processing library",
     author="Mathieu Moalic",
     author_email="matmoa@pm.me",
